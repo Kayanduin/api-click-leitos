202ab02a6c03fe89f7c49c2035f91d8f5e75c3e9
Rafael Farias <rafael.oliveirafa@gmail.com>
2021-09-29 23:33:09 -0300

    Correção no model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,22 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+
+class User extends Model
 {
-    use HasApiTokens;
     use HasFactory;
-    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var string[]
+     * @var array
      */
     protected array $fillable = [
         'name',
@@ -27,21 +23,27 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
     protected array $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be visible in arrays.
      *
      * @var array
      */
-    protected array $casts = [
-        'email_verified_at' => 'datetime',
+    protected array $visible = [
+        'id',
+        'name',
+        'email',
+        'password',
+        'cpf',
+        'created_at',
+        'updated_at'
     ];
+
 }
